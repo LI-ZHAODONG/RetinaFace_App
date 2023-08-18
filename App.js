2ae476f6519efc4211d1e8312c916c0ebb3d06b1
Daniel-Pacer <daniel-pacer@Daniel-Pacers-MacBook-Pro.local>
2023-08-18 22:19:12 +0800

    tensorflow

--- App.js
+++ App.js
@@ -1,8 +1,164 @@
-import * as React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { StyleSheet, Text, View, Dimensions, Platform } from 'react-native';
 
-import Home from './Pages/Home';
-import Cam from './Pages/Cam';
+import { Camera } from 'expo-camera';
+
+import * as tf from '@tensorflow/tfjs';
+import * as posedetection from '@tensorflow-models/pose-detection';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import {
+  bundleResourceIO,
+  cameraWithTensors,
+} from '@tensorflow/tfjs-react-native';
+import Svg, { Circle } from 'react-native-svg';
+import { ExpoWebGLRenderingContext } from 'expo-gl';
+import { CameraType } from 'expo-camera/build/Camera.types';
+
+const TensorCamera = cameraWithTensors(Camera);
+
+const IS_ANDROID = Platform.OS === 'android';
+const IS_IOS = Platform.OS === 'ios';
+
+const CAM_PREVIEW_WIDTH = Dimensions.get('window').width;
+const CAM_PREVIEW_HEIGHT = CAM_PREVIEW_WIDTH / (IS_IOS ? 9 / 16 : 3 / 4);
+
+const MIN_KEYPOINT_SCORE = 0.3;
+
+const OUTPUT_TENSOR_WIDTH = 180;
+const OUTPUT_TENSOR_HEIGHT = OUTPUT_TENSOR_WIDTH / (IS_IOS ? 9 / 16 : 3 / 4);
+
+const AUTO_RENDER = false;
+
+const LOAD_MODEL_FROM_BUNDLE = false;
 
 export default function App() {
-  return <Home />;
-};
+  const cameraRef = useRef(null);
+  const [tfReady, setTfReady] = useState(false);
+  const [model, setModel] = useState(null);
+  const [poses, setPoses] = useState(null);
+  const [fps, setFps] = useState(0);
+  const [orientation, setOrientation] = useState(null);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.front);
+  const rafId = useRef(null);
+
+  useEffect(() => {
+    async function prepare() {
+      rafId.current = null;
+
+      const curOrientation = await ScreenOrientation.getOrientationAsync();
+      setOrientation(curOrientation);
+
+      ScreenOrientation.addOrientationChangeListener((event) => {
+        setOrientation(event.orientationInfo.orientation);
+      });
+
+      await Camera.requestCameraPermissionsAsync();
+      await tf.ready();
+
+      const movenetModelConfig = {
+        modelType: posedetection.movenet.modelType.SINGLEPOSE_LIGHTNING,
+        enableSmoothing: true,
+      };
+
+      if (LOAD_MODEL_FROM_BUNDLE) {
+        const modelJson = require('./offline_model/model.json');
+        const modelWeights1 = require('./offline_model/group1-shard1of2.bin');
+        const modelWeights2 = require('./offline_model/group1-shard2of2.bin');
+        movenetModelConfig.modelUrl = bundleResourceIO(modelJson, [
+          modelWeights1,
+          modelWeights2,
+        ]);
+      }
+
+      const model = await posedetection.createDetector(
+        posedetection.SupportedModels.MoveNet,
+        movenetModelConfig
+      );
+      setModel(model);
+
+      setTfReady(true);
+    }
+
+    prepare();
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      if (rafId.current != null && rafId.current !== 0) {
+        cancelAnimationFrame(rafId.current);
+        rafId.current = 0;
+      }
+    };
+  }, []);
+
+  const handleCameraStream = async (
+    images,
+    updatePreview,
+    gl
+  ) => {
+    const loop = async () => {
+      const imageTensor = images.next().value;
+
+      const startTs = Date.now();
+      const poses = await model.estimatePoses(
+        imageTensor,
+        undefined,
+        Date.now()
+      );
+      const latency = Date.now() - startTs;
+      setFps(Math.floor(1000 / latency));
+      setPoses(poses);
+      tf.dispose([imageTensor]);
+
+      if (rafId.current === 0) {
+        return;
+      }
+
+      if (!AUTO_RENDER) {
+        updatePreview();
+        gl.endFrameEXP();
+      }
+
+      rafId.current = requestAnimationFrame(loop);
+    };
+
+    loop();
+  };
+
+  // The rest of the code remains the same...
+
+  if (!tfReady) {
+    return (
+      <View style={styles.loadingMsg}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  } else {
+    return (
+      <View
+        style={
+          isPortrait() ? styles.containerPortrait : styles.containerLandscape
+        }
+      >
+        <TensorCamera
+          ref={cameraRef}
+          style={styles.camera}
+          autorender={AUTO_RENDER}
+          type={cameraType}
+          resizeWidth={getOutputTensorWidth()}
+          resizeHeight={getOutputTensorHeight()}
+          resizeDepth={3}
+          rotation={getTextureRotationAngleInDegrees()}
+          onReady={handleCameraStream}
+        />
+        {renderPose()}
+        {renderFps()}
+        {renderCameraTypeSwitcher()}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  // The styles remain the same...
+});
